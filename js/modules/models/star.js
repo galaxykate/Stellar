063d16ca6ee809492bbbc03954ad43da3afd632f
Kate <galaxykate@gmail.com>
2013-06-22 12:49:34 -0700

    Moving the translation of the star into the UniverseView, where it belongs.  Also, stubbing in drawing multiple layers, and star-states

--- js/modules/models/star.js
+++ js/modules/models/star.js
@@ -7,6 +7,21 @@
 define(["inheritance", "modules/models/vector"], function(Inheritance, Vector) {
     return (function() {
 
+        var states = [{
+            name : "dust",
+            idNumber : 0,
+        }, {
+            name : "star",
+            idNumber : 1,
+        }, {
+            name : "nova",
+            idNumber : 2,
+        }];
+
+        var randomState = function() {
+            return states[Math.floor(Math.random() * 3)];
+        };
+
         // Private functions
         var starCount = 0;
 
@@ -24,10 +39,39 @@ define(["inheritance", "modules/models/vector"], function(Inheritance, Vector) {
             g.ellipse(this.position.y, this.position.x, 50, 50);
         };
 
+        function drawLayer(g, options) {
+            
+            switch(options.layer) {
+                case "bg":
+                    break;
+
+                case "main":
+                    var h = (this.idNumber * .212 + .3) % 1;
+                    g.fill(h, 1, 1);
+                    g.noStroke();
+                    g.ellipse(0, 0, this.radius, this.radius);
+                    break;
+
+                case "overlay":
+
+                    var h = (this.idNumber * .212 + .3) % 1;
+                    g.stroke(h, 1, 1);
+                    g.noFill();
+                    g.ellipse(0, 0, this.radius + 10, this.radius + 10);
+
+                    g.fill(h, 1, 1);
+                    g.text(this.state.name, this.radius, this.radius);
+                    break;
+            }
+        };
+
         // Make the star class
         var Star = Class.extend({
             init : function() {
+
                 this.idNumber = starCount;
+                this.state = randomState();
+                this.radius = Math.random() * 100 + 10;
                 starCount++;
                 initAsParticle(this);
                 this.position.setToPolar(Math.random() * 100, Math.random() * 100);
@@ -44,7 +88,7 @@ define(["inheritance", "modules/models/vector"], function(Inheritance, Vector) {
                 //   console.log(this.velocity);
             },
 
-            draw : testDraw,
+            draw : drawLayer,
         });
 
         return {
