8e0d89f10ae64a4c0e9c202d0a8fecec3f951c30
April Grow <grow.april@gmail.com>
2013-07-21 11:27:16 -0700

    working on other popup parts

--- js/modules/models/popup.js
+++ js/modules/models/popup.js
@@ -6,28 +6,40 @@
 
 define(["inheritance", "modules/models/vector"], function(Inheritance, Vector) {
     return (function() {
+    	
+    	var states = [{
+            name : "default",
+            idNumber : 0,
+            x : 0,
+            y : 0,
+            width: 10,
+            height: 10
+        }];
 
         var Popup = Class.extend({
 
             init : function() {
 				console.log("Init a popup!!!");
+				this.states = [];
             },
-
-            drawBackground : function(g, options) {
-
-            },
-
-            drawMain : function(g, options) {
-
-            },
-
-            drawOverlay : function(g, options) {
-
-            },
-
-            update : function(time) {
+            
+            addState : function(name, x, y, width, height) {
+            	var state = {
+            		"name": name,
+            		idNumber: this.states.length,
+            		"x": x,
+            		"y": y,
+            		"width": width,
+            		"height": height
+            	};
             	
+            	this.states[name].push(state);
+            },
+            
+            setState : function(name) {
+            	this.activeState = states[name];
             }
+            
         });
 
         return Popup;
