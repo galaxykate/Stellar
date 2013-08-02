5bd52750fd8b395a9f91b5a023f5ffa474e62ef0
Kate <galaxykate@gmail.com>
2013-08-02 11:30:38 -0700

    Adding a local settings file

---
+++ js/settings_local.js
@@ -0,0 +1,8 @@
+/**
+ * @author Kate Compton
+ */
+
+
+// Any unset value will be "undefined".  So either your default value should be false, 
+//  or make a reasonable default value in the settings file to be overridden by your own value
+settings.user = "Kate";
